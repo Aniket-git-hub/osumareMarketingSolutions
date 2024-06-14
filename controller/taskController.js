56eb594c4617fe7bd66b2d56cd7b7a89969aa965
aniket-git-hub <singhdharmvir81@gmail.com>
2024-06-14 20:53:34 +0530

    added controllers

---
+++ controller/taskController.js
@@ -0,0 +1,39 @@
+export function getTasks(req, res, next) {
+    try {
+
+    } catch (error) {
+        next(error)
+    }
+}
+
+export function getTaskById(req, res, next) {
+    try {
+
+    } catch (error) {
+        next(error)
+    }
+}
+
+export function createTask(req, res, next) {
+    try {
+
+    } catch (error) {
+        next(error)
+    }
+}
+
+export function updateTask(req, res, next) {
+    try {
+
+    } catch (error) {
+        next(error)
+    }
+}
+
+export function deleteTask(req, res, next) {
+    try {
+
+    } catch (error) {
+        next(error)
+    }
+}
--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express';
+import { createTask, deleteTask, getTaskById, getTasks, updateTask } from '../controller/taskController';
 
 const router = express.Router();
 
-router.get('/')
-router.get('/:id')
-router.post('/')
-router.put('/:id')
-router.delete('/:id')
+router.get('/', getTasks)
+router.get('/:id', getTaskById)
+router.post('/', createTask)
+router.put('/:id', updateTask)
+router.delete('/:id', deleteTask)
 
 export default router;
